c0b059cee570693fd285a2cd0466371e5fb4e7fb
AsadullahShaikh123 <asadullahshaikh713@gmail.com>
2022-06-26 23:19:23 -0700

    added partial data

---
+++ src/data.js
@@ -0,0 +1,446 @@
+
+import image1 from '../public/images/case_studies/broadcaster.webp'
+import image2 from '../public/images/case_studies/charity.webp'
+import image3 from '../public/images/case_studies/shopping.webp'
+import image4 from '../public/images/case_studies/super_market.webp'
+import image5 from '../public/images/case_studies/trigger_emails.webp'
+import image6 from '../public/images/case_studies/university.webp'
+import image7 from '../public/images/case_studies/web_design.webp'
+import image8 from '../public/images/case_studies/branding_agency.webp'
+import image9 from '../public/images/case_studies/consultancy.webp'
+import image10 from '../public/images/case_studies/seo.webp'
+import image11 from '../public/images/innovation/innovations.webp'
+import image12 from '../public/images/innovation/innovation_strategy.webp'
+import image13 from '../public/images/innovation/business_model.webp'
+import image14 from '../public/images/innovation/designer_sustainably.webp'
+import image15 from '../public/images/innovation/create_team.webp'
+import image16 from '../public/images/innovation/assess_company.webp'
+import image17 from '../public/images/innovation/self_steering.webp'
+import image18 from '../public/images/innovation/innovation_strategy.webp'
+import image19 from '../public/images/innovation/future_proof.webp'
+import image20 from '../public/images/management/easy_ways.webp'
+import image21 from '../public/images/management/leadership.webp'
+import image22 from '../public/images/management/business.webp'
+import image23 from '../public/images/management/technology.webp'
+import image24 from '../public/images/management/digital_disruptors.webp'
+import image25 from '../public/images/management/skill.webp'
+import image26 from '../public/images/management/time_energy.webp'
+import image27 from '../public/images/management/operations.webp'
+import image28 from '../public/images/management/challenges.webp'
+import image29 from '../public/images/advertising/markdown.webp'
+import image30 from '../public/images/advertising/cookie.webp'
+import image31 from '../public/images/advertising/guide.webp'
+import image32 from '../public/images/advertising/unlock_power.webp'
+import image33 from '../public/images/advertising/digital_marketing.webp'
+import image34 from '../public/images/advertising/debate.webp'
+import image35 from '../public/images/advertising/advertising.webp'
+import image36 from '../public/images/advertising/native_advertising.webp'
+import image37 from '../public/images/advertising/programmatic_outdoor.webp'
+import image38 from '../public/images/advertising/social_media.webp'
+import image39 from '../public/images/advertising/reactive_marketing.webp'
+
+let data =[
+  {
+    case_studies:[
+      {
+        category:'Case Studies',
+        articleHeading:'Broadcaster Saves Hours of Admin with Innovative Digital Audio Solution',
+        author:'Steve Grant',
+        date:'April 16, 2020',
+        read:'1 min read',
+        image:image1,
+        tags:['#agency','#business','#technology'],
+        icons:['f','t','in','e-mail']
+      },
+      {
+        category:'Case Studies',
+        articleHeading:'Charity engages 1m more supporters with revamped digital presence',
+        author:'John Doe',
+        date:'April 16, 2020',
+        read:'1 min read',
+        image:image2,
+        tags:['#agency','#business','#technology'],
+        icons:['f','t','in','e-mail']
+      },
+      {
+        category:'Case Studies',
+        articleHeading:'Online shopping data drives offline revenue growth of 410%',
+        author:'Jane Smith',
+        date:'April 16, 2020',
+        read:'1 min read',
+        image:image3,
+        tags:['#agency','#business','#technology'],
+        icons:['f','t','in','e-mail']
+      },
+      {
+        category:'Case Studies',
+        articleHeading:'Supermarket Chain’s Site Redesign Boosts Online Sales And Market Share',
+        author:'John Doe',
+        date:'April 16, 2020',
+        read:'1 min read',
+        image:image4,
+        tags:['#agency','#business','#technology'],
+        icons:['f','t','in','e-mail']
+      },
+      {
+        category:'Case Studies',
+        articleHeading:'Trigger emails boost digital revenue by 9.1% for furniture retailer',
+        author:'John Doe',
+        date :'April 16, 2020',
+        read:'1 min read',
+        image:image5,
+        tags:['#agency','business','#technology'],
+        icons:['f','t','in','e-mail']
+      },
+      {
+        category:'Case Studies',
+        articleHeading:'University boosts alumni engagement with nostalgic playlist',
+        author:'Steve Grant',
+        date:'April 16, 2020',
+        read:'1 min read',
+        image: image6,
+        tags:['#agency','business','#technology'],
+        icons:['f','t','in','e-mail']
+      },
+      {
+        category:'Case Studies',
+        articleHeading:'Web Design Company’s Secret to Not Losing Money on Projects',
+        author:'Jane Smith',
+        date:'April 16, 2020',
+        read:'1 min read',
+        image: image7,
+        tags:['#agency','business','#technology'],
+        icons:['f','t','in','e-mail']
+      },
+      {
+        category:'Case Studies',
+        articleHeading:'Branding Agency Replaced Ten Tools With Just One',
+        author:'Jane Smith',
+        date:'April 16, 2020',
+        read:'1 min read',
+        image: image8,
+        tags:['#agency','business','#technology'],
+        icons:['f','t','in','e-mail']
+      },
+      {
+        category:'Case Studies',
+        articleHeading:'Consultancy Saves 30% Time on Managing Daily Work',
+        author:'John Doe',
+        date:'April 16, 2020',
+        read:'1 min read',
+        image: image9,
+        tags:['#agency','business','#technology'],
+        icons:['f','t','in','e-mail']
+      },
+      {
+        category:'Case Studies',
+        articleHeading:'SEO and content campaign delivers 2,190% ROI',
+        author:'John Doe',
+        date:'April 16, 2020',
+        read:'1 min read',
+        image: image10,
+        tags:['#agency','business','#technology'],
+        icons:['f','t','in','e-mail']
+      }
+    ],
+    innovations:[
+      {
+        category:'Innovation',
+        articleHeading:'5 ingenious innovations triggered by chaos in startups',
+        author:'John Doe',
+        date:'April 02, 2020',
+        read:'1 min read',
+        image: image11,
+        tags:['business','#innovation'],
+        icons:['f','t','in','e-mail']
+      },
+      {
+        category:'Innovation',
+        articleHeading:'50 social impact innovations that might save the world',
+        author:'John Doe',
+        date:'April 02, 2020',
+        read:'1 min read',
+        image: image12,
+        tags:['business','#innovation'],
+        icons:['f','t','in','e-mail']
+      },
+      {
+        category:'Innovation',
+        articleHeading:'Business Model pivots in the Low Touch Economy',
+        author:'John Doe',
+        date:'April 02, 2020',
+        read:'1 min read',
+        image: image13,
+        tags:['business','#innovation'],
+        icons:['f','t','in','e-mail']
+      },
+      {
+        category:'Innovation',
+        articleHeading:'Can a designer sustainably transform the world?',
+        author:'John Doe',
+        date:'April 02, 2020',
+        read:'1 min read',
+        image: image14,
+        tags:['business','#innovation'],
+        icons:['f','t','in','e-mail']
+      },
+      {
+        category:'Innovation',
+        articleHeading:'Create an international team of innovation catalysts in 5 steps',
+        author:'John Doe',
+        date:'April 02, 2020',
+        read:'1 min read',
+        image: image15,
+        tags:['business','#innovation'],
+        icons:['f','t','in','e-mail']
+      },
+      {
+        category:'Innovaton',
+        articleHeading:'How to assess your company’s level of innovation',
+        author:'John Doe',
+        date:'April 02, 2020',
+        read:'1 min read',
+        image: image16,
+        tags:['#business','#innovation'],
+        icons:['f','t','in','e-mail']
+      },
+      {
+        category:'Innovation',
+        articleHeading:'How to maintain a self-steering culture while scaling up',
+        author:'John Doe',
+        date:'April 02, 2020',
+        read:'1 min read',
+        image:image17,
+        tags:['#business','#innovation'],
+        icons:['f','t','in','e-mail']
+      },
+      {
+        category:'Innovation',
+        articleHeading:'How to test whether your innovation strategy is socially responsible',
+        author:'John Doe',
+        date:'April 02, 2020',
+        read:'1 min read',
+        image:image18,
+        tags:['#business','#innovation'],
+        icons:['f','t','in','e-mail']
+      },
+      {
+        category:'Innovation',
+        articleHeading:'Future-proof your organization with biomimicry thinking',
+        author:'John Doe',
+        date:'April 02, 2020',
+        read:'1 min read',
+        image:image19,
+        tags:['#business','#innovatoin'],
+        icons:['f','t','in','e-mail']
+      }
+    ],
+    management:[
+      {
+        category:'Management',
+        articleHeading:'5 Easy Ways to Boost Your Team Performance Immediately',
+        author:'Steve Grant',
+        date:'April 16, 2020',
+        read:'1 min read',
+        image:image20,
+        tags:['#agency','#business','#technology'],
+        icons:['f','t','in','e-mail']
+      },
+      {
+        category:'Management',
+        articleHeading:'Effective Leadership in the Digital Age',
+        author:'John Doe',
+        date:'April 16, 2020',
+        read:'1 min read',
+        image:image21,
+        tags:['#agency','#business','#technology'],
+        icons:['f','t','in','e-mail']
+      },
+      {
+        category:'Management',
+        articleHeading:'How to Choose the Right Business Management Software',
+        author:'John Doe',
+        date:'April 16, 2020',
+        read:'1 min read',
+        image:image22,
+        tags:['#agency','#business','#technology'],
+        icons:['f','t','in','e-mail']
+      },
+      {
+        category:'Management',
+        articleHeading:'How to Use Technology to Increase Your Productivity',
+        author:'John Doe',
+        date:'April 16, 2020',
+        read:'1 min read',
+        image:image23,
+        tags:['#agency','business','#technology'],
+        icons:['f','t','in','e-mail']
+      },
+      {
+        category:'Management',
+        articleHeading:'Learning from Digital Disruptors Best Practice Guide',
+        author:'Steve Grant',
+        date:'April 16, 2020',
+        read:'1 min read',
+        image:image24,
+        tags:['#agency','business','#technology'],
+        icons:['f','t','in','e-mail']
+      },
+      {
+        category:'Management',
+        articleHeading:'People use the word skill to describe different things',
+        author:'Steve Grant',
+        date:'April 16, 2020',
+        read:'1 min read',
+        image:image25,
+        tags:['#agency','#business','#technology'],
+        icons:['f','t','in','e-mail']
+      },
+      {
+        category:'Management',
+        articleHeading:'Should You Manage Your Time or Your Energy?',
+        author:'Steve Grant',
+        date:'April 16, 2020',
+        read:'1 min read',
+        image: image26,
+        tags:['#agency','#business','#technology'],
+        icons:['f','t','in','e-mail']
+      },
+      {
+        category:'Management',
+        articleHeading:'This is What Poor Operations Could Be Costing You',
+        author:'John Doe',
+        date:'April 16, 2020',
+        read:'1 min read',
+        image:image27,
+        tags:['#agency','#business','#technology'],
+        icons:['f','t','in','e-mail']
+      },
+      {
+        category:'Management',
+        articleHeading:'Top Challenges for Digital Agencies in 2020',
+        author:'Jane Smith',
+        date:'April 16, 2020',
+        read:'1 min read',
+        image:image28,
+        tags:['#agency','#business','#technology'],
+        icons:['f','t','in','e-mail']
+      }
+    ],
+    advertising:[
+      {
+        category:'Advertising',
+        articleHeading:'Markdown Language Sample Blog Post Styling',
+        author:'Jane Smith',
+        date:'April 16, 2020',
+        read:'1 min read',
+        image:image29,
+        tags:['#agency','#business','#technology'],
+        icons:['f','t','in','e-mail']
+      },
+      {
+        category:'Advertising',
+        articleHeading:'An introduction to the third-party cookie crackdown',
+        author:'John Doe',
+        date:'April 16, 2020',
+        read:'1 min read',
+        image:image30,
+        tags:['#agency','#business','#technology'],
+        icons:['f','t','in','e-mail']
+      },
+      {
+        category:'Advertising',
+        articleHeading:'A Marketer’s Guide to Price Comparison Websites',
+        author:'Steve Grant',
+        date:'April 16, 2020',
+        read:'1 min read',
+        image:image31,
+        tags:['#agency','#business','#technology'],
+        icons:['f','t','in','e-mail']
+      },
+      {
+        category:'Advertising',
+        articleHeading:'Four ways to unlock the true power of TV through programmatic',
+        author:'John Doe',
+        date:'April 16, 2020',
+        read:'1 min read',
+        image:image32,
+        tags:['#agency','#business','#technology'],
+        icons:['f','t','in','e-mail']
+      },
+      {
+        category:'Advertising',
+        articleHeading:'The Best Digital Marketing Stats We’ve Seen This Week',
+        author:'John Doe',
+        date:'April 16, 2020',
+        read:'1 min read',
+        image:image33,
+        tags:['#agency','#business','#technology'],
+        icons:['f','t','in','e-mail']
+      },
+      {
+        category:'Advertising',
+        articleHeading:'The Consultancy Vs. Agency Debate It’s All About Effectiveness',
+        author:'Steve Grant',
+        date:'April 16, 2020',
+        read:'1 min read',
+        image:image34,
+        tags:['#agency','#business','#technology'],
+        icons:['f','t','in','e-mail']
+      },
+      {
+        category:'Advertising',
+        articleHeading:'Getting to Grips with Digital Advertising Best Practice Guide',
+        author:'John Doe',
+        date:'April 16, 2020',
+        read:'1 min read',
+        image:image35,
+        tags:['#agency','#business','#technology'],
+        icons:['f','t','in','e-mail']
+      },
+      {
+        category:'Advertising',
+        articleHeading:'The Power Of Native Advertising For Travel Brands',
+        author:'Steve Grant',
+        date:'April 16, 2020',
+        read:'1 min read',
+        image:image36,
+        tags:['#agency','#business','#technology'],
+        icons:['f','t','in','e-mail']
+      },
+      {
+        category:'Advertising',
+        articleHeading:'The Rise Of Programmatic Outdoor What Advertisers Need To Know',
+        author:'Steve Grant',
+        date:'April 16, 2020',
+        read:'1 min read',
+        image:image37,
+        tags:['#agency','#business','#technology'],
+        icons:['f','t','in','e-mail']
+      },
+      {
+        category:'Advertising',
+        articleHeading:'Top Social Media Platforms Overview',
+        author:'John Doe',
+        date:'April 16, 2020',
+        read:'1 min read',
+        image:image38,
+        tags:['#agency','#business','#technology'],
+        icons:['f','t','in','e-mail']
+      },
+      {
+        category:'Advertising',
+        articleHeading:'What Is Reactive Marketing? And Which Brands Do It Well?',
+        author:'Steve Grant',
+        date:'April 16, 2020',
+        read:'1 min read',
+        image:image39,
+        tags:['#agency','#business','#technology'],
+        icons:['f','t','in','e-mail']
+      }
+  ]
+    
+  }
+]
+
+export default data;
