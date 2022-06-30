eb4f39d59f1ee4a569a8d274c0f40689d437aef8
AsadullahShaikh123 <asadullahshaikh713@gmail.com>
2022-06-30 06:52:01 -0700

    done with advertising section

--- src/pages/home/BlogCards.js
+++ src/pages/home/BlogCards.js
@@ -1,10 +1,23 @@
 import React from 'react'
+import data from '../../data'
+import SingleCard from './SingleCard'
 
 const BlogCards = () => {
+ let advertise =  data[0].advertising.slice(0,3)
   return (
     <>
+    <div className="margin-top" style={{marginTop:'32px',border:'2px solid transparent'}}></div>
       <div className="blog-cards">
-        
+        <div className="blog-cards-margin w-1140 ml-auto mr-auto px-8">
+            <div className="blog-cards-content flex justify-between w-full">
+                {
+                  advertise.map(value => 
+                    <SingleCard category ={value.category} articleHeading={value.articleHeading} author = {value.author} date ={value.date} read={value.read} image={value.image} />
+                  )
+                }
+                              
+            </div>
+        </div>
       </div>
     </>
   )
---
+++ src/pages/home/SingleCard.js
@@ -0,0 +1,101 @@
+import React from "react";
+
+const SingleCard = ({
+  category,
+  articleHeading,
+  author,
+  date,
+  read,
+  image,
+}) => {
+  return (
+    <>
+      <div
+        className="single-card w-1/3 p-2"
+      >
+        <div className="bg-white rounded-lg "
+        style={{ boxShadow: "1px 1px 5px 0 rgb(1 1 1 / 5%)"}} >
+
+        
+        <a href="#image" className="flex">
+          <div style={{ margin: "8px 8px 0 8px", borderRadius: "8px" }}>
+            <img src={image} alt="" srcset="" style={{ borderRadius: "8px" }} />
+          </div>
+        </a>
+        <div className="content" style={{ padding: "32px" }}>
+          <div className="button" style={{ marginBottom: "16px" }}>
+            <a
+              href="#button"
+              style={{
+                padding: "4px 16px",
+                backgroundColor: "#e9d8fd",
+                fontWeight: "600",
+                color: "#2d3748",
+                borderRadius: "8px",
+                fontSize: "12px",
+              }}
+            >
+              {category}
+            </a>
+          </div>
+          <a
+            href="#heading"
+            style={{
+              fontSize: "20px",
+              fontWeight: "700",
+              color: "rgb(45, 55, 72)",
+              marginBottom: "16px",
+              display: "flex",
+            }}
+          >
+            {articleHeading}
+          </a>
+          <p
+            style={{
+              marginBottom: "16px",
+              fontSize: "14px",
+              color: "rgb(113, 128, 150)",
+              lineHeight: "25.2px",
+            }}
+          >
+            Markdown is a lightweight markup language with plain-text-formatting
+            syntax. Its design allows it to…
+          </p>
+          <a href="author-intro" className="author-intro flex">
+            <div style={{ marginRight: "16px" }}>
+              <div style={{ width: "48px", height: "48px" }}>
+                <img
+                  src={image}
+                  alt=""
+                  srcset=""
+                  style={{
+                    width: "100%",
+                    height: "100%",
+                    borderRadius: "100%",
+                  }}
+                />
+              </div>
+            </div>
+            <div>
+              <p
+                style={{
+                  fontSize: "14px",
+                  fontWeight: "700",
+                  color: "rgb(102, 126, 234)",
+                }}
+              >
+                {author}
+              </p>
+              <p style={{ fontSize: "14px", color: " rgb(160, 174, 192)" }}>
+                {date} {read}
+              </p>
+            </div>
+          </a>
+        </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default SingleCard;
